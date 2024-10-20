82959d0cd7026b0b7f46125f3c1c0eacead11b7d
PauloAlmeidaBrasa <almeida.desenvolve@gmail.com>
2024-10-20 19:03:52 -0300

    feat: add kenex and dependecnies

---
+++ knexfile.ts
@@ -0,0 +1,19 @@
+import { Knex  } from 'knex';
+
+const config: Knex.Config = {
+  client: 'mysql2',
+  connection: {
+    host: 'localhost',
+    user: 'admin',
+    password: 'adminpassword',
+    database: 'admin-stroll'
+  },
+  migrations: {
+    directory: 'src/main/database/migrations'
+  },
+  seeds: {
+    directory: 'src/main/database/seeds'
+  }
+};
+
+export default config;
